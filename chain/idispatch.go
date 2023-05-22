2b6c04cb073e0e2a92dd8587ca4b73c338330a54
FleetingTimePaul <83365885@qq.com>
2023-05-22 22:39:16 +0800

    Optimize.

--- chain/idispatch.go
+++ chain/idispatch.go
@@ -4,7 +4,4 @@ import "github.com/MysGate/demo_backend/model"
 
 type IDispatcher interface {
 	PayForDest(*model.Order) error
-	GenerateZkProof(*model.Order) error
-	VerifyZkProof(*model.Order) error
-	OrderSucceed(*model.Order) error
 }
